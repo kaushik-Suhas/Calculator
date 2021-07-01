70321099c03e6ddf34c6beb53a5cca148353eead
kaushikSuhas94 <kaushiksuhas94@gmail.com>
2021-07-01 23:03:48 +0530

    step 6

--- angular-calculator-app/src/app/display/display.component.ts
+++ angular-calculator-app/src/app/display/display.component.ts
@@ -4,34 +4,41 @@ import { AppService } from '../app.service';
 @Component({
   selector: 'app-display',
   templateUrl: './display.component.html',
-  styleUrls: ['./display.component.css']
+  styleUrls: ['./display.component.css'],
 })
 export class DisplayComponent implements OnInit {
   key1: string = '';
   key2: string = '';
   operator: string = '';
-  result: number| undefined;
+  result: string = '';
 
-  constructor(private appService: AppService) { }
+  constructor(private appService: AppService) {}
 
   ngOnInit(): void {
-    this.appService.keyAdded.subscribe(
-      (val: string) => {
-        if( val == '+' || val == '-' || val == '*' || val == '/' ) {
-          this.operator = val;
-        } else if (!this.operator && val !== '=' && val !== 'C') {
-          this.key1 = this.key1 + val;
-        } else if (this.operator && val !== '=' && val !== 'C') {
-          this.key2 = this.key2 + val; 
-        } else if (val == '='){
-          this.result = this.appService.calculation(this.key1, this.key2, this.operator);
-        } else if (val == 'C') {
-          console.log('here'),
-          this.key1 = '',
-          this.key2 = '',
-          this.result = 0;
-        }
-     }
-)
-}
+    this.appService.keyAdded.subscribe((val: string) => {
+      if (val == '+' || val == '-' || val == '*' || val == '/') {
+        this.operator = val;
+      } else if (!this.operator && val !== '=' && val !== 'C') {
+        this.key1 = this.key1 + val;
+      } else if (this.operator && val !== '=' && val !== 'C') {
+        this.key2 = this.key2 + val;
+      } else if (val == '=') {
+        this.result = this.appService.calculation(
+          this.key1,
+          this.key2,
+          this.operator
+        );
+      } else if (val == 'C') {
+          this.key1 = '';
+          this.key2 = '';
+          this.result = '';
+          this.operator = '';
+          console.log('here');
+      } else if (this.operator == val) {
+        this.key1 = this.result;
+        this.key2 = '';
+        console.log('here');
+      }
+    });
+  }
 }
