48281bd3b330f3ec2386bc5705a9c3ec4003abc6
kaushikSuhas94 <kaushiksuhas94@gmail.com>
2021-07-01 21:45:44 +0530

    setp 5

--- angular-calculator-app/src/app/app.service.ts
+++ angular-calculator-app/src/app/app.service.ts
@@ -3,4 +3,10 @@ import { EventEmitter, Injectable } from "@angular/core";
 @Injectable({providedIn: 'root'})
 export class AppService {
   keyAdded = new EventEmitter<string>();
+
+  calculation(key1: string, key2: string, operator: string) {
+     const exp = key1 + operator + key2
+     return eval(exp)    
+  }
+  
 }
--- angular-calculator-app/src/app/display/display.component.ts
+++ angular-calculator-app/src/app/display/display.component.ts
@@ -1,4 +1,3 @@
-import { keyframes } from '@angular/animations';
 import { Component, KeyValueDiffers, OnInit } from '@angular/core';
 import { AppService } from '../app.service';
 
@@ -8,18 +7,31 @@ import { AppService } from '../app.service';
   styleUrls: ['./display.component.css']
 })
 export class DisplayComponent implements OnInit {
-  keys: string|undefined = '';
+  key1: string = '';
+  key2: string = '';
+  operator: string = '';
+  result: number| undefined;
 
   constructor(private appService: AppService) { }
 
   ngOnInit(): void {
-    // this.keys = this.appService.onkeyValueAdded();
     this.appService.keyAdded.subscribe(
-      (keyValue: string) => {
-        console.log('in here')
-        this.keys = this.keys + keyValue;
-      }
-    )
-  }
-
+      (val: string) => {
+        if( val == '+' || val == '-' || val == '*' || val == '/' ) {
+          this.operator = val;
+        } else if (!this.operator && val !== '=' && val !== 'C') {
+          this.key1 = this.key1 + val;
+        } else if (this.operator && val !== '=' && val !== 'C') {
+          this.key2 = this.key2 + val; 
+        } else if (val == '='){
+          this.result = this.appService.calculation(this.key1, this.key2, this.operator);
+        } else if (val == 'C') {
+          console.log('here'),
+          this.key1 = '',
+          this.key2 = '',
+          this.result = 0;
+        }
+     }
+)
+}
 }
