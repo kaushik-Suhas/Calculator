30444fe9ef979571d8e84c4bdd1681a50c42e95c
kaushikSuhas94 <kaushiksuhas94@gmail.com>
2021-07-03 17:21:43 +0530

    step 9

--- angular-calculator-app/src/app/display/display.component.ts
+++ angular-calculator-app/src/app/display/display.component.ts
@@ -1,4 +1,5 @@
-import { Component, KeyValueDiffers, OnInit } from '@angular/core';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
+import { Component, OnInit } from '@angular/core';
 import { AppService } from '../app.service';
 
 @Component({
@@ -11,6 +12,7 @@ export class DisplayComponent implements OnInit {
   key2: string = '';
   operator: string = '';
   result: string = '';
+  resultCheck: boolean = false;
 
   constructor(private appService: AppService) {}
 
@@ -21,7 +23,11 @@ export class DisplayComponent implements OnInit {
         if(!this.key1 && val == '-'){
           console.log('here')
           this.key1 = this.key1 + val;
-        } else if(this.operator) {
+        } else if(!this.key2 && this.operator){
+          this.result = 'Error'
+          console.log('here in')
+        }
+        else if(this.operator) {
           this.result = this.appService.calculation(this.key1, this.key2, this.operator)
           this.key1 = this.result;
           this.result = '';
@@ -31,22 +37,32 @@ export class DisplayComponent implements OnInit {
           this.operator = val;
         }
       } else if (!this.operator && val !== '=' && val !== 'C') {
-        console.log('inside key1')
-        this.key1 = this.key1 + val;
+        if( val == '0' ){
+          if(this.key1 !== ''){
+            this.key1 += val;
+          }
+        } else {
+          this.key1 += val;
+        } 
+        console.log(this.key1, 'key1')
       } else if (this.operator && val !== '=' && val !== 'C') {
         this.key2 = this.key2 + val;
-        console.log('inside key2')
-      } else if (val == '=') {
+        console.log(this.key2, 'inside key2')
+      } else if (val == '=') { console.log('in here') 
         this.result = this.appService.calculation(
           this.key1,
           this.key2,
-          this.operator
+          this.operator,  
         );
+        if(this.result == '0') {
+          this.resultCheck = true;
+        }
       } else if (val == 'C') {
           this.key1 = '';
           this.key2 = '';
           this.result = '';
           this.operator = '';
+          this.resultCheck = false;
       }
     });
   }
